d4486293b9ceef224e7b32473678578fe561a93f
Travis CI User <travis@example.org>
2017-01-21 14:04:14 +0000

    Update documentation

---
+++ implementors/jsonrpc_core/calls/trait.Metadata.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["ethcore_rpc"] = ["impl Metadata for <a class='struct' href='ethcore_rpc/struct.Metadata.html' title='ethcore_rpc::Metadata'>Metadata</a>",];implementors["ethcore_dapps"] = ["impl Metadata for <a class='struct' href='ethcore_rpc/v1/metadata/struct.Metadata.html' title='ethcore_rpc::v1::metadata::Metadata'>Metadata</a>",];implementors["parity"] = ["impl Metadata for <a class='struct' href='ethcore_rpc/v1/metadata/struct.Metadata.html' title='ethcore_rpc::v1::metadata::Metadata'>Metadata</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
